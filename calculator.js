e9d501c227dddc6f10744422c72b94e86250839d
Leight Johnson <leightjohnson93@gmail.com>
2018-09-25 16:40:32 -0400

    passed all tests

---
+++ calculator.js
@@ -0,0 +1,46 @@
+class Calculator {
+  constructor() {
+    this.stack = [];
+  }
+  checkEmpty() {if (this.stack.length === 0) {
+    throw 'calculator is empty'
+  }
+}
+  value() {
+    if (this.stack.length === 0) {
+      return 0;
+    }
+    return this.stack[this.stack.length-1];
+  }
+
+  enter(value) {
+    this.stack.push(value);
+  }
+
+  plus() {
+    this.checkEmpty();
+    this.stack.push(this.stack.pop() + this.stack.pop());
+  }
+
+  minus() {
+    this.checkEmpty();
+    let last = this.stack.pop();
+    let penult = this.stack.pop() || 0;
+    this.stack.push(penult - last);
+  }
+
+  divide() {
+    this.checkEmpty();
+    let last = this.stack.pop();
+    let penult = this.stack.pop();
+    this.stack.push(penult / last);
+  }
+
+  times() {
+    this.checkEmpty();
+    let last = this.stack.pop();
+    let penult = this.stack.pop();
+    this.stack.push(penult * last);
+  }
+}
+module.exports = Calculator;
